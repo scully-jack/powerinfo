148c4d89e3d62e14ed1955573b8d44853282dde6
scully-jack <155685713+scully-jack@users.noreply.github.com>
2024-01-05 09:16:08 +1100

    Update powerinfoap.py

--- powerinfoap.py
+++ powerinfoap.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python
+pip install snowflake-sqlalchemy
+pip install numpy
+pip install pandas
+pip install snowflake
 from snowflake.sqlalchemy import URL
 from sqlalchemy import create_engine
 from sqlalchemy import delete
 import numpy as np
 import pandas as pd
 from snowflake.connector.pandas_tools import write_pandas
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-import datetime
-from datetime import timedelta
-import xlsxwriter
-import pathlib
-import os
-import sys
-import win32com.client
-import shutil
-import pythoncom
-import re
-import time
 
 user_name = 'JACK.SCULLY@KMART.COM.AU'
 
